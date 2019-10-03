746b62971923c47e38e89be52134b002b16097c2
sunil178 <sunilthakur178co@gmail.com>
2019-10-03 18:33:38 +0530

    2D Array - DS

---
+++ 2d_array_DS.py
@@ -0,0 +1,16 @@
+# 2D Array - DS
+
+
+arr = [[-9, -9, -9, 1, 1, 1], [0, -9, 0, 4, 3, 2], [-9, -9, -9, 1, 2, 3], [0, 0, 8, 6, 6, 0], [0, 0, 0, -2, 0, 0], [0, 0, 1, 2, 4, 0]]
+i = 0
+total = 0
+hourglass = []
+while i < len(arr) - 2:
+    j = 0
+    while j < len(arr) - 2:
+        total += arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+        hourglass.append(total)
+        total = 0
+        j += 1
+    i += 1
+print(max(hourglass))
