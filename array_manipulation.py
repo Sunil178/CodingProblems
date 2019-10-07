c6edd0ec39f4e22d52aba4973b205bd22e82a372
sunil178 <sunilthakur178co@gmail.com>
2019-10-07 15:06:01 +0530

    Updated Array Manipulation

--- array_manipulation.py
+++ array_manipulation.py
@@ -1,13 +1,17 @@
 # Array Manipulation
 
-lst = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-add = [3, 7, 1]
-rg = [[1, 5], [4, 8], [6, 9]]
+rg = list(map(int, input().split()))
+lst= []
+for i in range(rg[0]):
+    lst.append(0)
+add = []
+for j in range(rg[1]):
+    add.append(list(map(int, input().split())))
 i = 0
-while i < len(rg):
-    j = rg[i][0] - 1
-    while j < rg[i][1]:
-        lst[j] += add[i]
+while i < len(add):
+    j = add[i][0] - 1
+    while j < add[i][1]:
+        lst[j] += add[i][2]
         j += 1
     i += 1
 print(max(lst))
