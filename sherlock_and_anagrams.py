935f9489bedfcc588a2fae1856254e028b01809e
sunil178 <sunilthakur178co@gmail.com>
2019-10-13 11:36:08 +0530

    Sherlock and Anagrams

---
+++ sherlock_and_anagrams.py
@@ -0,0 +1,24 @@
+# Sherlock and Anagrams
+
+string = "cdcd"
+k = 0
+anagrams = 0
+while k < len(string)-1:
+    i = 0
+    while i < len(string):
+        j = i + 1
+        while j < len(string)-k:
+            str1 = string[i:i+(k+1)]
+            str2 = string[j:j+(k+1)]
+            c = 0
+            if len(str1) == len(str2):
+                for char in str1:
+                    if str1.count(char) == str2.count(char):
+                        c += 1
+                if c == len(str1):
+                    print(str1, ",", str2)
+                    anagrams += 1
+            j += 1
+        i += 1
+    k += 1
+print(anagrams)
